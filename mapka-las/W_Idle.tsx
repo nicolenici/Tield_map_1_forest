cd4dc800adf8ddf8b3f6c79ab74c6b38c4bc8c68
nicolenici <n-busch@o2.pl>
2025-03-28 22:51:06 +0100

    Tutorial map
    The final version of the map was created in Tiled. It is designed for a tutorial where players will learn how to move and chop trees. At the end of the map, there is a cave where the game continues.

---
+++ mapka-las/W_Idle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="W_Idle" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="Tiny Swords (Update 010)/Resources/Resources/W_Idle.png" width="128" height="128"/>
+</tileset>
---
+++ mapka-las/kolekcja2.tsx
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="kolekcja2" tilewidth="640" tileheight="512" tilecount="53" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="maly_muchomorek">
+  <image source="Tiny Swords (Update 010)/Deco/01.png" width="64" height="64"/>
+ </tile>
+ <tile id="1" type="sredni_muchomorek">
+  <image source="Tiny Swords (Update 010)/Deco/02.png" width="64" height="64"/>
+ </tile>
+ <tile id="2" type="duzy_muchomorek">
+  <image source="Tiny Swords (Update 010)/Deco/03.png" width="64" height="64"/>
+ </tile>
+ <tile id="3" type="maly_kamyk">
+  <image source="Tiny Swords (Update 010)/Deco/04.png" width="64" height="64"/>
+ </tile>
+ <tile id="4" type="maly_krzak">
+  <image source="Tiny Swords (Update 010)/Deco/07.png" width="64" height="64"/>
+ </tile>
+ <tile id="5" type="sredni_krzak">
+  <image source="Tiny Swords (Update 010)/Deco/08.png" width="64" height="64"/>
+ </tile>
+ <tile id="6" type="sredni_kamyk">
+  <image source="Tiny Swords (Update 010)/Deco/05.png" width="64" height="64"/>
+ </tile>
+ <tile id="7" type="duzy_kamyk">
+  <image source="Tiny Swords (Update 010)/Deco/06.png" width="64" height="64"/>
+ </tile>
+ <tile id="8" type="duzy_krzak">
+  <image source="Tiny Swords (Update 010)/Deco/09.png" width="64" height="64"/>
+ </tile>
+ <tile id="9" type="male_liscie">
+  <image source="Tiny Swords (Update 010)/Deco/10.png" width="64" height="64"/>
+ </tile>
+ <tile id="10" type="duze_liscie">
+  <image source="Tiny Swords (Update 010)/Deco/11.png" width="64" height="64"/>
+ </tile>
+ <tile id="11" type="brzydka_dynia">
+  <image source="Tiny Swords (Update 010)/Deco/12.png" width="64" height="64"/>
+ </tile>
+ <tile id="12" type="dynia">
+  <image source="Tiny Swords (Update 010)/Deco/13.png" width="64" height="64"/>
+ </tile>
+ <tile id="13" type="kosc">
+  <image source="Tiny Swords (Update 010)/Deco/14.png" width="64" height="64"/>
+ </tile>
+ <tile id="14" type="pol_kosci">
+  <image source="Tiny Swords (Update 010)/Deco/15.png" width="64" height="64"/>
+ </tile>
+ <tile id="15" type="znak1">
+  <image source="Tiny Swords (Update 010)/Deco/16.png" width="64" height="128"/>
+ </tile>
+ <tile id="16" type="znak2">
+  <image source="Tiny Swords (Update 010)/Deco/17.png" width="64" height="128"/>
+ </tile>
+ <tile id="17" type="strach_na_wroble">
+  <image source="Tiny Swords (Update 010)/Deco/18.png" width="192" height="192"/>
+ </tile>
+ <tile id="18">
+  <image source="Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_House/Goblin_House.png" width="128" height="192"/>
+ </tile>
+ <tile id="19">
+  <image source="Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_House/Goblin_House_Destroyed.png" width="128" height="192"/>
+ </tile>
+ <tile id="20">
+  <image source="Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Destroyed.png" width="256" height="192"/>
+ </tile>
+ <tile id="21">
+  <image source="Tiny Swords (Update 010)/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_InConstruction.png" width="256" height="192"/>
+ </tile>
+ <tile id="22">
+  <image source="Tiny Swords (Update 010)/Terrain/Bridge/Bridge_All.png" width="192" height="256"/>
+ </tile>
+ <tile id="23">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Shadows.png" width="192" height="192"/>
+ </tile>
+ <tile id="24">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation.png" width="256" height="512"/>
+ </tile>
+ <tile id="28">
+  <image source="Tiny Swords (Update 010)/Resources/Gold Mine/GoldMine_Active.png" width="192" height="128"/>
+ </tile>
+ <tile id="29">
+  <image source="Tiny Swords (Update 010)/Resources/Gold Mine/GoldMine_Destroyed.png" width="192" height="128"/>
+ </tile>
+ <tile id="30">
+  <image source="Tiny Swords (Update 010)/Resources/Gold Mine/GoldMine_Inactive.png" width="192" height="128"/>
+ </tile>
+ <tile id="25" x="0" y="0" width="192" height="192" type="trawa">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Flat — kopia.png" width="640" height="256"/>
+ </tile>
+ <tile id="34" type="trawa1">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Flat — kopia.png" width="640" height="256"/>
+ </tile>
+ <tile id="45">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation1.png" width="167" height="167"/>
+ </tile>
+ <tile id="46">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation2.png" width="168" height="168"/>
+ </tile>
+ <tile id="47">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation3.png" width="168" height="168"/>
+ </tile>
+ <tile id="48">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation5.png" width="168" height="168"/>
+ </tile>
+ <tile id="49">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation6.png" width="168" height="168"/>
+ </tile>
+ <tile id="50">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation7.png" width="168" height="168"/>
+ </tile>
+ <tile id="51">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation8.png" width="168" height="168"/>
+ </tile>
+ <tile id="52">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation11.png" width="168" height="84"/>
+ </tile>
+ <tile id="53">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation12.png" width="168" height="84"/>
+ </tile>
+ <tile id="54">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation13.png" width="168" height="168"/>
+ </tile>
+ <tile id="55">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation14.png" width="168" height="168"/>
+ </tile>
+ <tile id="56">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation15.png" width="192" height="64"/>
+ </tile>
+ <tile id="57">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation21.png" width="84" height="84"/>
+ </tile>
+ <tile id="58">
+  <image source="Tiny Swords (Update 010)/Terrain/Ground/Tilemap_Elevation22.png" width="82" height="84"/>
+ </tile>
+ <tile id="59">
+  <image source="Tiny Swords (Update 010)/Factions/Knights/Buildings/House/House_Yellow.png" width="128" height="192"/>
+ </tile>
+ <tile id="61">
+  <image source="Tiny Swords (Update 010)/Terrain/Water/Water.png" width="64" height="64"/>
+ </tile>
+ <tile id="67">
+  <image source="Tiny Swords (Update 010)/Resources/Trees/Tree.png" width="135" height="181"/>
+ </tile>
+</tileset>
